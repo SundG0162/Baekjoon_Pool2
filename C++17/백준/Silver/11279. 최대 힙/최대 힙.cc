ff50acb3a829aaefe0fc42548f8bba68ac3d3b85
SundG0162 <janghyunwoo0162@gmail.com>
2024-10-23 15:21:56 +0900

    [Silver II] Title: 최대 힙, Time: 16 ms, Memory: 2916 KB -BaekjoonHub

---
+++ C++17/백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <queue>
+#include <string>
+#include <cstring>
+#include <set>
+
+using namespace std;
+
+int main()
+{
+	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+
+	priority_queue<int, vector<int>, less<int>> q;
+	int n;
+	cin >> n;
+	int x;
+	for (int i = 0; i < n; i++)
+	{
+		cin >> x;
+		if (x == 0)
+		{
+			if (q.empty())
+			{
+				cout << 0 << '\n';
+				continue;
+			}
+			cout << q.top() << '\n';
+			q.pop();
+		}
+		else
+		{
+			q.push(x);
+		}
+	}
+}
