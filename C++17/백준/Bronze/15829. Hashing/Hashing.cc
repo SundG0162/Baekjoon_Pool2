b04503a1cb24e72f512a68a50ed247a61186ab23
SundG0162 <janghyunwoo0162@gmail.com>
2024-11-02 18:00:32 +0900

    [Bronze II] Title: Hashing, Time: 0 ms, Memory: 2024 KB, Score: 100 point -BaekjoonHub

--- C++17/백준/Bronze/15829. Hashing/Hashing.cc
+++ C++17/백준/Bronze/15829. Hashing/Hashing.cc
@@ -9,6 +9,16 @@ using namespace std;
 const int R = 31;
 const int M = 1234567891;
 
+long long int pow_m(int x, int y, int m)
+{
+	long long int v = 1;
+	for (int i = 0; i < y; i++)
+	{
+		v = (v * x) % m;
+	}
+	return v % m;
+}
+
 int main()
 {
 	ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
@@ -16,11 +26,11 @@ int main()
 	cin >> l;
 	string str;
 	cin >> str;
-	int hash = 0;
+	long long int hash = 0;
 	for (int i = 0; i < str.size(); i++)
 	{
-		hash += (str[i] - 'a' + 1) * pow(R, i);
+		hash += ((str[i] - 'a' + 1) * pow_m(R, i, M));
 	}
 
-	cout << hash;
+	cout << hash % M;
 }
