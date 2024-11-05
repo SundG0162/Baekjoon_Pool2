66a99fa749715e5955510288e4390e4492dd2655
SundG0162 <janghyunwoo0162@gmail.com>
2024-11-06 08:48:58 +0900

    [Silver II] Title: 스택 수열, Time: 40 ms, Memory: 3368 KB -BaekjoonHub

---
+++ C++17/백준/Silver/1874. 스택 수열/스택 수열.cc
@@ -0,0 +1,56 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <string>
+#include <cstring>
+
+using namespace std;
+
+int main()
+{
+	int n;
+	cin >> n;
+	int m = 1;
+	bool flag = false;
+	string str = "";
+	stack<int> s;
+	vector<int> result;
+
+	for (int i = 0, x; i < n; i++)
+	{
+		cin >> x;
+		if (flag) continue;
+		while (true)
+		{
+			if (!s.empty() && s.top() == x)
+			{
+				result.push_back(s.top());
+				s.pop();
+				str += "-\n";
+				break;
+			}
+			else if (!s.empty() && s.top() != x && x < m)
+			{
+				flag = true;
+				str = "NO";
+				break;
+			}
+			else
+			{
+				str += "+\n";
+				s.push(m);
+				if (x == m)
+				{
+					result.push_back(s.top());
+					s.pop();
+					str += "-\n";
+					m++;
+					break;
+				}
+				m++;
+			}
+		}
+	}
+	cout << str;
+}
