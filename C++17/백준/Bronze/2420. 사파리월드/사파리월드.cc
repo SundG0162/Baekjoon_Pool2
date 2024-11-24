02b98695ee582c6d56dfde122e1a562d1d395b41
SundG0162 <janghyunwoo0162@gmail.com>
2024-11-25 07:56:53 +0900

    [Bronze V] Title: 사파리월드, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2420. 사파리월드/사파리월드.cc
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<queue>
+#include<algorithm>
+#include<cmath>
+#include<map>
+
+using namespace std;
+
+int main()
+{
+    long long a, b;
+    cin >> a >> b;
+    cout << abs(a - b);
+}
