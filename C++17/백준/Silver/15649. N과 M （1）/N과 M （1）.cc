e42c359b7a65b06aa9da6b01f7190079d9ce9831
SundG0162 <janghyunwoo0162@gmail.com>
2025-03-24 15:44:12 +0900

    [Silver III] Title: N과 M (1), Time: 24 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -0,0 +1,51 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<queue>
+#include<stack>
+#include<algorithm>
+#include<sstream>
+#include<cmath>
+#include<map>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::queue;
+using std::stack;
+using std::string;
+
+int n, m;
+bool visited[9];
+
+vector<int> sequence;
+
+void N_and_M()
+{
+	if (sequence.size() == m)
+	{
+		for (int i = 0; i < sequence.size(); i++)
+			cout << sequence[i] << " ";
+		cout << '\n';
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (!visited[i])
+		{
+			visited[i] = true;
+			sequence.push_back(i);
+			N_and_M();
+			sequence.pop_back();
+			visited[i] = false;
+		}
+	}
+}
+
+int main()
+{
+	cin >> n >> m;
+	memset(visited, false, sizeof(visited));
+	N_and_M();
+}
