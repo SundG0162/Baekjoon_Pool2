516bb773cd61b242c4f30d7211eb6e719ee455c4
SundG0162 <janghyunwoo0162@gmail.com>
2025-04-06 17:18:10 +0900

    [Gold V] Title: 두 용액, Time: 60 ms, Memory: 2412 KB -BaekjoonHub

---
+++ C++20/백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,57 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<queue>
+#include<stack>
+#include<algorithm>
+#include<sstream>
+#include<cmath>
+#include<map>
+#include<unordered_map>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::queue;
+using std::stack;
+using std::string;
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> solutionVec(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		cin >> solutionVec[i];
+	}
+
+	sort(solutionVec.begin(), solutionVec.end(), std::less<int>());
+
+	int left = 0, right = n - 1;
+
+	std::pair<int, int> minPair(solutionVec[0], solutionVec[1]);
+
+	while (right > left)
+	{
+		int curMin = minPair.second + minPair.first;
+		int newMin = solutionVec[right] + solutionVec[left];
+		if (std::abs(newMin) < std::abs(curMin))
+		{
+			minPair.first = solutionVec[left];
+			minPair.second = solutionVec[right];
+		}
+		if(newMin > 0)
+		{
+			right--;
+		}
+		else
+		{
+			left++;
+		}
+	}
+
+	cout << minPair.first << " " << minPair.second;
+}
