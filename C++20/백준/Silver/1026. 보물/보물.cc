25da4ebae1902d04296f0d73cf48e09faa437778
SundG0162 <janghyunwoo0162@gmail.com>
2025-04-15 17:13:33 +0900

    [Silver IV] Title: 보물, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++20/백준/Silver/1026. 보물/보물.cc
@@ -0,0 +1,45 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<sstream>
+#include<cmath>
+#include<map>
+#include<unordered_map>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::queue;
+using std::deque;
+using std::stack;
+using std::string;
+
+#define FAST_PRINT std::ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define BEGIN_TO_END(vec) vec.begin(), vec.end()
+#define MEMSET(arr, i) memset(arr, i, sizeof(arr))
+
+int main()
+{
+	int n;
+	cin >> n;
+	vector<int> a(n), b(n);
+	for (int i = 0; i < n; i++)
+		cin >> a[i];
+	for (int i = 0; i < n; i++)
+		cin >> b[i];
+
+	std::sort(a.begin(), a.end(), std::greater<int>());
+	std::sort(b.begin(), b.end(), std::less<int>());
+
+	int s = 0;
+	for (int i = 0; i < n; i++)
+	{
+		s += a[i] * b[i];
+	}
+	cout << s;
+}
