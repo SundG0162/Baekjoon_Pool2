7f7d28d91df92c8172f8c3b2950e8bc160924306
SundG0162 <janghyunwoo0162@gmail.com>
2025-04-16 14:53:48 +0900

    [Silver III] Title: 이친수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Silver/2193. 이친수/이친수.cc
@@ -0,0 +1,46 @@
+#include<iostream>
+#include<string>
+#include<cstring>
+#include<queue>
+#include<deque>
+#include<stack>
+#include<algorithm>
+#include<sstream>
+#include<cmath>
+#include<map>
+#include<unordered_map>
+
+using std::cin;
+using std::cout;
+using std::endl;
+using std::vector;
+using std::queue;
+using std::deque;
+using std::stack;
+using std::string;
+using std::pair;
+
+#define FAST_PRINT std::ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define BEGIN_TO_END(vec) vec.begin(), vec.end()
+#define RBEGIN_TO_REND(vec) vec.rbegin(), vec.rend()
+#define MEMSET(arr, i) memset(arr, i, sizeof(arr))
+#define MEMCPY(to, arr) memcpy(to, arr, sizeof(arr))
+
+
+unsigned long long dp[91];
+
+int main()
+{
+	int n;
+	cin >> n;
+	dp[0] = 0;
+	dp[1] = 1;
+	dp[2] = 1;
+
+	for (int i = 3; i <= n; i++)
+	{
+		dp[i] = dp[i - 1] + dp[i - 2];
+	}
+
+	cout << dp[n];
+}
