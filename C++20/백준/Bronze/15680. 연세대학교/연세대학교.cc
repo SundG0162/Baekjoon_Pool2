024f53d9bc3ba7d2547ad1b52600bfdea2c680e1
SundG0162 <janghyunwoo0162@gmail.com>
2025-04-24 08:49:45 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int input;
+    cin >> input;
+    if(input == 0)
+        cout << "YONSEI";
+    else
+        cout << "Leading the Way to the Future";
+}
