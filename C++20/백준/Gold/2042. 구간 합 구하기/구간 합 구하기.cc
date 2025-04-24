d2785da1615cb0aa4a0fff9aa85dec9cfef236a0
SundG0162 <janghyunwoo0162@gmail.com>
2025-04-24 22:43:37 +0900

    [Gold I] Title: 구간 합 구하기, Time: 192 ms, Memory: 41084 KB -BaekjoonHub

---
+++ C++20/백준/Gold/2042. 구간 합 구하기/구간 합 구하기.cc
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define FAST_PRINT std::ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define BEGIN_TO_END(vec) vec.begin(), vec.end()
+#define MEMSET(arr, i) memset(arr, i, sizeof(arr))
+
+long long sequence[1000001];
+long long segTree[4000001];
+
+
+// 초기값 설정
+long long build(int node, int start, int end)
+{
+	if (start == end)
+	{
+		segTree[node] = sequence[start];
+		return segTree[node];
+	}
+
+	int mid = (start + end) / 2;
+
+	long long leftSum = build(node * 2, start, mid);
+	long long rightSum = build(node * 2 + 1, mid + 1, end);
+
+	segTree[node] = leftSum + rightSum;
+	return segTree[node];
+}
+
+//현재 보고 있는 노드, 현재 노드가 담당 중인 구간, 합을 구하고자 하는 구간
+long long query(int node, int start, int end, int qStart, int qEnd)
+{
+	if (end < qStart || start > qEnd)
+		return 0;
+	if (start >= qStart && end <= qEnd)
+		return segTree[node];
+
+	int mid = (start + end) / 2;
+	long long leftChild = query(node * 2, start, mid, qStart, qEnd);
+	long long rightChild = query(node * 2 + 1, mid + 1, end, qStart, qEnd);
+	return leftChild + rightChild;
+}
+
+void update(int node, int start, int end, int target, long long value)
+{
+	if (target < start || target > end)
+		return;
+	if (start == end)
+	{
+		segTree[node] = value;
+		return;
+	}
+
+	int mid = (start + end) / 2;
+
+	update(node * 2, start, mid, target, value);
+	update(node * 2 + 1, mid + 1, end, target, value);
+
+	segTree[node] = segTree[node * 2] + segTree[node * 2 + 1];
+}
+
+
+int main()
+{
+	FAST_PRINT;
+
+	int n, m, k;
+	cin >> n >> m >> k;
+
+	for (int i = 1; i <= n; i++)
+	{
+		cin >> sequence[i];
+	}
+
+	build(1, 1, n);
+
+	for (long long i = 0, a, b, c; i < m + k; i++)
+	{
+		cin >> a >> b >> c;
+
+		if (a == 1)
+		{
+			update(1, 1, n, b, c);
+		}
+		else if (a == 2)
+		{
+			cout << query(1, 1, n, b, c) << '\n';
+		}
+	}
+}
