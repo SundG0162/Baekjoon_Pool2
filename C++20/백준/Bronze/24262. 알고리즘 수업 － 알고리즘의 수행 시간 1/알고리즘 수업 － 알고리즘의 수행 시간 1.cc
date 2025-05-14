187cb0634f78d6f09a2ef7de237d1e3b1a9be592
SundG0162 <janghyunwoo0162@gmail.com>
2025-05-14 23:08:26 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -0,0 +1,18 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define FAST_PRINT std::ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define BEGIN_TO_END(vec) vec.begin(), vec.end()
+#define MEMSET(arr, i) memset(arr, i, sizeof(arr))
+#define INF 1e9
+
+int main()
+{
+	FAST_PRINT;
+	 
+	int n;
+	cin >> n;
+	cout << 1 << '\n';
+	cout << 0;
+}
