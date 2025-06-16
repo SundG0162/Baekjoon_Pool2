374ba99cfe208fbeabdfb80bc78e8dae6f805900
SundG0162 <janghyunwoo0162@gmail.com>
2025-06-16 22:17:36 +0900

    [Platinum V] Title: 약 팔기, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++20/백준/Platinum/15311. 약 팔기/약 팔기.cc
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+
+using namespace std;
+
+#define FAST_PRINT std::ios_base::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL)
+#define BEGIN_TO_END(vec) vec.begin(), vec.end()
+#define MEMSET(arr, i) memset(arr, i, sizeof(arr))
+
+int main()
+{
+	FAST_PRINT;
+
+	int n;
+	cin >> n;
+
+	cout << 2000 << '\n';
+	for (int i = 0; i < 1000; i++)
+		cout << 1 << " ";
+	for (int i = 0; i < 1000; i++)
+		cout << 1000 << " ";
+}
